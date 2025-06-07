69befe3ebc18abe752c608473833740f0df4dfe9
lkata22 <146066281+lkata22@users.noreply.github.com>
2025-06-07 18:20:25 +0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,24 @@
+import pandas as pd
+from torchvision import transforms
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+
+class FERDataset(Dataset):
+    def __init__(self, csv_file, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        image = np.fromstring(self.data.iloc[idx]['pixels'], sep=' ', dtype=np.uint8).reshape(48, 48)
+        image = Image.fromarray(image).convert("L")
+        label = int(self.data.iloc[idx]['emotion'])
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        return image, label
 
